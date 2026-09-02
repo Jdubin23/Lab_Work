74e69a6b3ef65a3a3546b4db5534f7dd7a60f74b
tiptonblu3 <tiptonblu3@gmail.com>
2026-09-02 19:12:07 -0400

    Bishop added
    It went away

---
+++ Lab 1/Assets/Chess/ChessGrid.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+public class ChessGrid : MonoBehaviour
+{
+    
+    private int x = 8; // Counter for the number of squares drawn
+    private int z = 8; // Counter for the number of squares drawn
+
+    private void OnDrawGizmos()
+    {
+        
+    }
+    
+}
