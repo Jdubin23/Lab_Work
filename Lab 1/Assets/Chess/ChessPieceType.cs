3efa995c498bbe4ea183cce8dd056e75788dfc47
tiptonblu3 <tiptonblu3@gmail.com>
2026-09-03 16:19:20 -0400

    King Move Drawn
    Script draws the kings moves.

--- Lab 1/Assets/Chess/ChessPieceType.cs
+++ Lab 1/Assets/Chess/ChessPieceType.cs
@@ -11,16 +11,17 @@ public class ChessType : MonoBehaviour
     public Sprite[] Sprites = new Sprite[6];
     public SpriteRenderer spriteRenderer;
     public Transform endPoint;
+    public GameObject piece;
 
     [Header("Directions")]
-    [HideInInspector] public Vector3 N = new Vector3(0, 1, 0).normalized;
-    [HideInInspector] public Vector3 NE = new Vector3(1, 1, 0).normalized;
-    [HideInInspector] public Vector3 E = new Vector3(1, 0, 0).normalized;
-    [HideInInspector] public Vector3 SE = new Vector3(1, -1, 0).normalized;
-    [HideInInspector] public Vector3 S = new Vector3(0, -1, 0).normalized;
-    [HideInInspector] public Vector3 SW = new Vector3(-1, -1, 0).normalized;
-    [HideInInspector] public Vector3 W = new Vector3(-1, 0, 0).normalized;
-    [HideInInspector] public Vector3 NW = new Vector3(-1, 1, 0).normalized;
+    [HideInInspector] public Vector3 N = new Vector3(0, 1, 0);
+    [HideInInspector] public Vector3 NE = new Vector3(1, 0, 1);
+    [HideInInspector] public Vector3 E = new Vector3(1, 0, 0);
+    [HideInInspector] public Vector3 SE = new Vector3(1, 0, -1);
+    [HideInInspector] public Vector3 S = new Vector3(0, -1, 0);
+    [HideInInspector] public Vector3 SW = new Vector3(-1, 0, -1);
+    [HideInInspector] public Vector3 W = new Vector3(-1, 0, 0);
+    [HideInInspector] public Vector3 NW = new Vector3(-1, 0, 1);
 
     public int pieceType = 6; // will be used for switch case to determine what chess piece it is.
     public float stepDistance = 1f; // Distance to move in each direction
@@ -61,6 +62,7 @@ public class ChessType : MonoBehaviour
                     DrawDirectionArrow(E); DrawDirectionArrow(W); 
                     DrawDirectionArrow(S);  DrawDirectionArrow(SW);
                      DrawDirectionArrow(NW);
+                     DrawKingMove();
                     break;
                 case 1: //Queen
                     DrawDirectionArrow(N);  DrawDirectionArrow(NE);
@@ -84,7 +86,7 @@ public class ChessType : MonoBehaviour
     private void DrawDirectionArrow(Vector3 directionOffset)
     {
         Vector3 start = transform.position;
-        Vector3 target = start + (directionOffset.normalized * stepDistance);
+        Vector3 target = start + (directionOffset * stepDistance);
 
         // Draw line connecting start to target
         Gizmos.DrawLine(start, target);
@@ -92,7 +94,7 @@ public class ChessType : MonoBehaviour
         #if UNITY_EDITOR
         // Draw an arrowhead pointing toward target using Editor Handles
         Handles.color = pieceColor;
-        Vector3 dir = (target - start).normalized;
+        Vector3 dir = (target - start);
         if (dir != Vector3.zero)
         {
             Quaternion rotation = Quaternion.LookRotation(dir, Vector3.forward);
@@ -101,6 +103,21 @@ public class ChessType : MonoBehaviour
         #endif
     }
 
+    private void DrawKingMove()
+    {
+        Gizmos.color = Color.blue;
+        //Vector3 position = transform.position;
+        
+        Gizmos.DrawCube(transform.position + transform.forward * 5, Vector3.one);
+        Gizmos.DrawCube(transform.position + transform.right * 5, Vector3.one);
+        Gizmos.DrawCube(transform.position + transform.forward * -5, Vector3.one);
+        Gizmos.DrawCube(transform.position + transform.right * -5, Vector3.one);
+        Gizmos.DrawCube(transform.position + transform.forward * 5 + transform.right * 5, Vector3.one);
+        Gizmos.DrawCube(transform.position + transform.forward * 5 + transform.right * -5, Vector3.one);
+        Gizmos.DrawCube(transform.position + transform.forward * -5 + transform.right * 5, Vector3.one);
+        Gizmos.DrawCube(transform.position + transform.forward * -5 + transform.right * -5, Vector3.one);
+
+    }
 
 
 }
